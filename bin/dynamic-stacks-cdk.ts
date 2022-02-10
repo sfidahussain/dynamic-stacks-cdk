a63738b12e30fc5eb3c61a7d578ec11a51fc1090
sfidahussain <sanfidahussain@gmail.com>
2022-02-09 19:40:26 -0600

    fixing

--- bin/dynamic-stacks-cdk.ts
+++ bin/dynamic-stacks-cdk.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+import * as cdk from '@aws-cdk/core';
 import { DynamicStacksCdkStack } from '../lib/dynamic-stacks-cdk-stack';
 import app from '../app.json';
 
--- lib/dynamic-stacks-cdk-stack.ts
+++ lib/dynamic-stacks-cdk-stack.ts
@@ -1,11 +1,11 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import * as cdk from '@aws-cdk/core';
 import { Construct } from 'constructs';
 import {Service} from "./service";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as apigateway from "@aws-cdk/aws-apigateway";
 
-export class DynamicStacksCdkStack extends Stack {
-  constructor(scope: Construct, id: string, service: Service, props?: StackProps) {
+export class DynamicStacksCdkStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, service: Service, props?: cdk.StackProps) {
     super(scope, id, props);
     const api = new apigateway.RestApi(this, 'books-api');
 
@@ -15,11 +15,5 @@ export class DynamicStacksCdkStack extends Stack {
     books.addMethod('GET');
     books.addMethod('POST');
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'DynamicStacksCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
